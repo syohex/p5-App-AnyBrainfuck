9d6961b33e580e0c93519bce83086ef78d3ad94d
Syohei YOSHIDA <syohex@gmail.com>
2011-11-15 16:09:37 +0900

    add function for creating config file with File::Temp

--- t/Util.pm
+++ t/Util.pm
@@ -3,7 +3,27 @@ use warnings;
 
 use base qw(Exporter);
 
+use File::Temp ();
+
+our @EXPORT_OK = qw(create_config_file);
+
+my @ops = (">", "<", "+", "-", ".", ",", "[", "]");
+
 sub create_config_file {
+    my %conf = @_;
+
+    my $tmp_conf = File::Temp->new;
+
+    my $str = "+{\n";
+    map {
+        $str .= "'$_' => '" . $conf{$_} . "',\n";
+    } grep { defined $conf{$_} } @ops, 'separator';
+    $str .= "}\n";
+
+    print {$tmp_conf} $str;
+    $tmp_conf->autoflush;
+
+    return $tmp_conf;
 }
 
 1;
